4ba217c48b86b06b1313a84360e7241f85155e60
DevOrenge <samuel.orwoll@gmail.com>
2022-05-21 12:59:05 +0200

    initial commit

---
+++ Assets/Picker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Picker : MonoBehaviour {
+	Camera cam;
+	Collider picked;
+
+	private void Start() {
+		cam = Camera.main;
+	}
+
+	private void Update() {
+		Ray mp = cam.ScreenPointToRay(Input.mousePosition);
+		RaycastHit rh;
+		
+		if(Physics.Raycast(mp, out rh) && rh.distance > 1.0f) {
+			picked = rh.collider;
+		} else {
+			picked = null;
+		}
+	}
+
+	private void OnDrawGizmos() {
+		if(picked != null) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawCube(picked.bounds.center, picked.bounds.size);
+		}
+	}
+}
